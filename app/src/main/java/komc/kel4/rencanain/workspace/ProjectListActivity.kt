c05b2e334713616f4ba435dc66925dc0b1efc98e
Yohana Septamia <yohanaseptamia@gmail.com>
2024-11-10 17:22:39 +0700

    FE project list

---
+++ app/src/main/java/komc/kel4/rencanain/workspace/ProjectListActivity.kt
@@ -0,0 +1,39 @@
+package komc.kel4.rencanain.workspace
+
+//Workspace: Yosep
+//1. Daftar Projek / Halaman Proyek
+//2. Detail projek
+//3. Detail tambah subtugas
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import komc.kel4.rencanain.R
+
+class ProjectListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_project_list)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Back button
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}

8dbb607d86ec7ae80b7cc7f8ffd9f5f416f0d401
frankverquez <saputrafrengky641@gmail.com>
2024-11-12 19:23:18 +0700

    Halaman home done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,11 +37,14 @@ android {
 
 dependencies {
 
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

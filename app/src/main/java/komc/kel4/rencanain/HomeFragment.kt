9f621502045e76ba304f487a7845f6e467a78d8d
Yohana Septamia <yohanaseptamia@gmail.com>
2024-12-11 06:16:07 +0700

    set button listener untuk pindah halaman/fragmen

--- app/src/main/java/komc/kel4/rencanain/HomeFragment.kt
+++ app/src/main/java/komc/kel4/rencanain/HomeFragment.kt
@@ -5,28 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import komc.kel4.rencanain.workspace.ProjectListFragment
+import komc.kel4.rencanain.jadwalku.MyScheduleFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -34,26 +19,33 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        fun setButtonClickListener(buttonId: Int, fragment: Fragment, menuId: Int) {
+            val button = view.findViewById<View>(buttonId)
+            button.setOnClickListener {
+                replaceFragment(fragment)
+                (requireActivity() as MainActivity).setActiveNavItem(menuId)
+            }
+        }
+
+        setButtonClickListener(R.id.btnProject_HOME, ProjectListFragment(), R.id.menuProjects)
+        setButtonClickListener(R.id.btnMySchedule_HOME, MyScheduleFragment(), R.id.menuSchedules)
+
+        return view
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
             }
     }
 }

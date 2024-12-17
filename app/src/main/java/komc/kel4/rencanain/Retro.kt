1c6832d5db3fccbed21f63f5fea68f9b8fa33533
MeliaPrnmsr <meliaprnmsr@gmail.com>
2024-12-17 15:33:04 +0700

    userprofile

--- app/src/main/java/komc/kel4/rencanain/Retro.kt
+++ app/src/main/java/komc/kel4/rencanain/Retro.kt
@@ -1,16 +1,38 @@
 package komc.kel4.rencanain
 
 import com.google.gson.GsonBuilder
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class Retro {
-    fun getRetroClientInstance(): Retrofit {
-        val gson = GsonBuilder().setLenient().create()
+//    fun getRetroClientInstance(): Retrofit {
+//        val gson = GsonBuilder().setLenient().create()
+//        return Retrofit.Builder()
+//            .baseUrl("http://192.168.125.165:8000/api/")
+//            .addConverterFactory(GsonConverterFactory.create(gson))
+//            .build()
+//    }
+
+    fun getRetroClientInstance(token: String? = null): Retrofit {
+        val clientBuilder = OkHttpClient.Builder()
+
+        if (token != null) {
+            val authInterceptor = Interceptor { chain ->
+                val request: Request = chain.request().newBuilder()
+                    .addHeader("Authorization", "Bearer $token")
+                    .build()
+                chain.proceed(request)
+            }
+            clientBuilder.addInterceptor(authInterceptor)
+        }
+
         return Retrofit.Builder()
-            .baseUrl("http://192.168.1.5:8000/api/")
-            .addConverterFactory(GsonConverterFactory.create(gson))
+            .baseUrl("http://192.168.244.165:8000/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(clientBuilder.build())
             .build()
     }
-
 }
--- app/src/main/java/komc/kel4/rencanain/UserApi.kt
+++ app/src/main/java/komc/kel4/rencanain/UserApi.kt
@@ -1,13 +1,22 @@
 package komc.kel4.rencanain
 
 import retrofit2.Call
+import retrofit2.http.Header
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.GET
 
 interface UserApi {
+    // login endpoint
     @POST("login")
     fun login(
         @Body userRequest: UserRequest
+    ): Call<UserResponse>
+
+    // userprofile endpoint
+    @GET("userprofile")
+    fun userprofile(
+        @Header("Authorization") token: String
     ): Call<UserResponse>
 
 }
